a2ff092bce59435cf4fcb47ea134d6b4a377f9dd
arcelen <arcelen.apk@gmail.com>
2024-05-11 22:17:17 +0100

    player & computer choice section completed

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log("hello");
+
+const options =["rock","paper","scissors"];
+
+function getComputerChoice(){
+    const choice= options[Math.floor(Math.random() * options.length)];
+    return choice;
+   
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection =="rock" && computerSelection == "scissors") || 
+        (playerSelection =="scissors" && computerSelection == "paper") || 
+        (playerSelection =="paper" && computerSelection == "rock")
+    ){
+        return "Player"
+    }
+    else{
+        return "Computer"
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection,computerSelection);
+    if (result=="Tie"){
+        return "It's a Tie!"
+    }
+    else if(result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+
+    }
+}
+function getPlayerChoice(){
+    let validatedInput =false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+    }
+}
+
+function game(){
+    console.log("Welcome")
+    for(let i=0 ;i < 5; i++){
+        const playerSelection = "rock";
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+    }
+}
+game()
